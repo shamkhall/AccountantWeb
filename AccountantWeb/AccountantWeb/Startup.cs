4390e0c597d9c0c6ae907d9df9405fd2a657089d
jacking07 <70231829+shamkhall@users.noreply.github.com>
2021-07-14 09:46:27 -0700

    test

--- AccountantWeb/AccountantWeb/Startup.cs
+++ AccountantWeb/AccountantWeb/Startup.cs
@@ -23,7 +23,7 @@ namespace AccountantWeb
     public class Startup
     {
         private IConfiguration _config;
-
+        
         public Startup(IConfiguration config)
         {
             _config = config;
